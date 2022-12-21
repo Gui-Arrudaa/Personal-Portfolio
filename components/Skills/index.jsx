cf2a2ada56a7f13fd64f93b555038439fdff2bb5
Guilherme <guipanfiglio123@outlook.com>
2022-12-21 19:13:21 -0300

    Create Skills Component

---
+++ components/Skills/index.jsx
@@ -0,0 +1,75 @@
+import {
+  SiGit,
+  SiGithub,
+  SiHtml5,
+  SiCss3,
+  SiSass,
+  SiJavascript,
+  SiReact,
+  SiNextdotjs,
+  SiPython,
+  SiMicrosoftoffice,
+  SiYarn,
+  SiNpm,
+} from 'react-icons/si';
+import SkillsContainer from './index.styled';
+
+const Skills = () => {
+  return (
+    <SkillsContainer>
+      <h2>Essas são algumas das minhas Competências!</h2>
+      <ul>
+        <li>
+          <SiHtml5 className="icon" />
+          <p>HTML5</p>
+        </li>
+        <li>
+          <SiCss3 className="icon" />
+          <p>CSS3</p>
+        </li>
+        <li>
+          <SiJavascript className="icon" />
+          <p>JavaScript</p>
+        </li>
+        <li>
+          <SiSass className="icon" />
+          <p>Sass</p>
+        </li>
+        <li>
+          <SiReact className="icon" />
+          <p>React</p>
+        </li>
+        <li>
+          <SiNextdotjs className="icon" />
+          <p>Next.Js</p>
+        </li>
+        <li>
+          <SiPython className="icon" />
+          <p>Python</p>
+        </li>
+        <li>
+          <SiGit className="icon" />
+          <p>Git</p>
+        </li>
+        <li>
+          <SiGithub className="icon" />
+          <p>GitHub</p>
+        </li>
+        <li>
+          <SiNpm className="icon" />
+          <p>Npm</p>
+        </li>
+        <li>
+          <SiYarn className="icon" />
+          <p>Yarn</p>
+        </li>
+        <li>
+          <SiMicrosoftoffice className="icon" />
+          <p>Office</p>
+        </li>
+      </ul>
+    </SkillsContainer>
+  );
+};
+
+export default Skills;
---
+++ components/Skills/index.styled.jsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+const SkillsContainer = styled.div`
+  padding: 2.5rem 5rem;
+  background: linear-gradient(180deg, rgba(182,45,236,1) 25%, rgba(140,25,185,1) 75%);
+  color: #f6f8fa;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 991.98px) {
+    padding: 2rem 2.5rem;
+  }
+
+  @media screen and (max-width: 991.98px) {
+    padding: 2rem;
+  }
+
+  h2 {
+    @media screen and (max-width: 620px) {
+      font-size: 20px;
+    }
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    max-width: 1500px;
+    flex-wrap: wrap;
+    row-gap: 2rem;
+    column-gap: 6rem;
+    margin-top: 3rem;
+
+    @media screen and (max-width: 991.98px) {
+      row-gap: 1rem;
+      column-gap: 3rem;
+      margin-top: 2rem;
+    }
+
+    @media screen and (max-width: 620px) {
+      column-gap: 2rem;
+    }
+
+    li {
+      .icon {
+        margin-bottom: 1rem;
+        font-size: 80px;
+        transition: all 0.2s;
+
+        &:hover {
+          transform: scale(1.2);
+        }
+
+        @media screen and (max-width: 1200px) {
+          font-size: 60px;
+        }
+
+        @media screen and (max-width: 1060px) {
+          font-size: 50px;
+        }
+
+        @media screen and (max-width: 991.98px) {
+          font-size: 40px;
+        }
+
+        @media screen and (max-width: 620px) {
+          font-size: 30px;
+        }
+      }
+
+      p {
+        font-weight: 600;
+      }
+    }
+  }
+`;
+
+export default SkillsContainer;
