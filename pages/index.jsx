a5b9a6e876c37544de7017c27f55a46b898b84e8
Guilherme <guipanfiglio123@outlook.com>
2022-12-22 21:16:03 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,19 @@
+import Navbar from '../components/Navbar/index';
+import About from '../components/About/index';
+import Skills from '../components/Skills/index';
+import Projects from '../components/Projects/index';
+import Socials from '../components/Socials';
+
 const Home = () => {
-  return <h1>Olá, Mundo.</h1>;
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Socials />
+    </>
+  );
 };
 
 export default Home;
