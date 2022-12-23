68be56d213279562af862dbff08dca6ec89f4224
Guilherme <guipanfiglio123@outlook.com>
2022-12-22 21:15:59 -0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,13 @@
+import GlobalStyle from '../styles/GlobalStyle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
